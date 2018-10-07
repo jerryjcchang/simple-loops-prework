4c020ce8aacad4cb5334ae7ce1e8f952dda29ae6
JC Chang <jerryjcchang@gmail.com>
2018-10-07 21:34:26 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -27,8 +27,8 @@ end
 def until_iterator(number_of_times)
     until number_of_times == 7
     puts "Welcome to Flatiron School's Web Development Course!"
-
-# end
+    end
+  end
 
 # def for_iterator(number_of_times)
 #   # code your solution here using the "for" keyword
