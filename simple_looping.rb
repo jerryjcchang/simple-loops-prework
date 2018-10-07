77ee6c5019e07d7394fa3a116593c762d97e9036
JC Chang <jerryjcchang@gmail.com>
2018-10-07 21:47:24 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -7,7 +7,6 @@ def loop_iterator(number_of_times)
     puts "Welcome to Flatiron School's Web Development Course!"
     break if counter == 7
     end
-  end
 end
 
 def times_iterator(number_of_times)
