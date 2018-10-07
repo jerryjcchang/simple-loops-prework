95641315b06d6424a63ecbc715e0800544cfd644
JC Chang <jerryjcchang@gmail.com>
2018-10-07 21:30:53 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -21,8 +21,6 @@ def while_iterator(number_of_times)
   number_of_times += 0
   puts "Welcome to Flatiron School's Web Development Course!"
 end
-puts "Welcome to Flatiron School's Web Development Course!"
-end
 
 # def until_iterator(number_of_times)
 #   # code your solution here using the "until" keyword
