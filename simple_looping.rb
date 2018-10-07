b60223682175a23e124d04c686140307974967d1
JC Chang <jerryjcchang@gmail.com>
2018-10-07 21:34:01 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -25,8 +25,8 @@ end
 end
 
 def until_iterator(number_of_times)
-until number_of_times == 7
-#   phrase = "Welcome to Flatiron School's Web Development Course!"
+    until number_of_times == 7
+    puts "Welcome to Flatiron School's Web Development Course!"
 
 # end
 
